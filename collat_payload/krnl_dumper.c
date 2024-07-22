c79a2d338cfdf5b8fe9be49e0ad92485fd4b917c
Harry <23122389+imcraze@users.noreply.github.com>
2024-07-22 17:09:45 +0100

    Module dumper working properly (excluding ntoskrnl.exe)

--- collat_payload/krnl_dumper.c
+++ collat_payload/krnl_dumper.c
@@ -48,6 +48,13 @@ char* read_null_terminated_string(UINT64 addr) {
     }
 }
 
+// should get header address for ntoskrnl.exe (currently doesn't work)
+// perhaps xor keys(?) change each reboot
+UINT64 krnl_header_address(UINT64 baseAddress, UINT64 data) {
+    UINT64 addrOfData = _byteswap_uint64(baseAddress ^ _rotl64(data ^ 0xB0837D93C0205F6E, 110)) ^ 0x1CD8854010D69B96;
+    return addrOfData;
+}
+
 int dump_bin(SOCKET s, char* name, UINT64 base, SIZE_T size) { // just for testing
     CHAR ptr_msg[1024] = { 0 };
 
@@ -87,307 +94,11 @@ int dump_bin(SOCKET s, char* name, UINT64 base, SIZE_T size) { // just for testi
     return 0;
 }
 
-int dump_kmodule2(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInfo, BOOL forceDumpInit) {
-    CHAR ptr_msg[1024] = { 0 };
-
-    if (strcmp(name, "ntoskrnl.exe") == 0) { // will take a look at once driver dumper works for normal drivers
-        return 1337;
-        sock_log(s, "[?] treating ntoskrnl specially :)\n");
-
-        // should dump approximately the bounds of the .text segment? hopefully?
-        //dump_bin(s, "ntoskrnl.exe_0x5cd000", (UINT64)moduleInfo.Base + 0x5ab000 - 0x3a5000, 0x195000 + 0x3a5000); // largest size without crash -- 0x5cd000, 0x173000 -- 0x5bb000, 0x185000
-        
-        //dump_bin(s, "shared_system_page", 0xfffff78000000000, 0x1000);
-        //dump_bin(s, "hal_loader_mappings", 0xffffffffffc00000, 0x80000);
-
-        dump_bin(s, name, moduleInfo.Base, moduleInfo.Size); // might take a while
-        return 1337;
-        unsigned char dump[64] = { 0 };
-        sock_log(s, "[?] Reading ntoskrnl header... ");
-        if (krnl_read((UINT64)moduleInfo.Base, dump, sizeof(dump)) != 0) {
-            sock_log(s, "Failed!\n");
-            return 2;
-        }
-        sock_log(s, "Done!\n");
-
-        //char out[256] = { 0 }; // probably should malloc this ngl
-        //hex_dump(dump, sizeof(dump), out);
-
-        //sprintf_s(ptr_msg,
-        //    sizeof(ptr_msg),
-        //    "[?] ntoskrnl.exe headers (?):\n%s\n", out);
-        //sock_log(s, ptr_msg);
-
-        //for (SIZE_T i = 0; i < sizeof(dump); i++) {
-        //    sprintf_s(ptr_msg,
-        //        sizeof(ptr_msg),
-        //        "%hhx", dump[i]);
-        //    sock_log(s, ptr_msg);
-        //}
-        //sock_log(s, "\n");
-
-        return 1337;
-    }
-
-    dump_bin(s, name, moduleInfo.Base, moduleInfo.Size);
-
-    
-
-    /*SIZE_T headersSize = 4096 / 2; // should contain headers
-    char* headersBuffer = (char*)malloc(headersSize);
-    if (!headersBuffer) {
-        sock_log(s, "[!] Failed to allocate headers buffer.\n");
-        return 1;
-    }*/
-
-    IMAGE_DOS_HEADER dosHeader;
-    sock_log(s, "[?] Reading DOS header... ");
-    if (krnl_read(moduleInfo.Base, &dosHeader, sizeof(IMAGE_DOS_HEADER)) != 0) {
-        sock_log(s, "Failed!\n");
-        return 2;
-    }
-    sock_log(s, "Done!\n");
-
-    if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE) {
-        sock_log(s, "[!] Invalid DOS signature.\n");
-        return 2;
-    }
-
-    IMAGE_NT_HEADERS64 ntHeader;
-    sock_log(s, "[?] Reading NT header... ");
-    if (krnl_read((UINT64)moduleInfo.Base + dosHeader.e_lfanew, &ntHeader, sizeof(IMAGE_NT_HEADERS64)) != 0) {
-        sock_log(s, "Failed!\n");
-        return 2;
-    }
-    sock_log(s, "Done!\n");
-
-    if (ntHeader.Signature != IMAGE_NT_SIGNATURE) {
-        sock_log(s, "[!] Invalid NT signature.\n");
-        return 2;
-    }
-
-    SIZE_T sectionHeaderTableSize = sizeof(IMAGE_SECTION_HEADER) * ntHeader.FileHeader.NumberOfSections;
-    PIMAGE_SECTION_HEADER sectionHeader = (PIMAGE_SECTION_HEADER)malloc(sectionHeaderTableSize);
-    sprintf_s(ptr_msg,
-        sizeof(ptr_msg),
-        "[?] Reading section header table at 0x%llx... ", (UINT64)moduleInfo.Base + dosHeader.e_lfanew + 0x24 + ntHeader.FileHeader.SizeOfOptionalHeader);
-    sock_log(s, ptr_msg);
-    if (krnl_read((UINT64)moduleInfo.Base + dosHeader.e_lfanew + sizeof(IMAGE_NT_HEADERS), sectionHeader, sectionHeaderTableSize) != 0) {
-        sock_log(s, "Failed!\n");
-        return 2;
-    }
-    
-    sock_log(s, "Done!\n");
-
-    /*PIMAGE_EXPORT_DIRECTORY exports = (PIMAGE_EXPORT_DIRECTORY)malloc(sizeof(IMAGE_EXPORT_DIRECTORY));
-    krnl_read((UINT64)moduleInfo.Base + ntHeader.OptionalHeader.DataDirectory[0].VirtualAddress, exports, sizeof(IMAGE_EXPORT_DIRECTORY));
-
-    PDWORD nameTable = (PDWORD)malloc(exports->NumberOfNames);
-    krnl_read((UINT64)moduleInfo.Base + exports->AddressOfNames, nameTable, exports->NumberOfNames);
-
-    sprintf_s(ptr_msg,
-        sizeof(ptr_msg),
-        "[?] Export table has %i functions and %i names. Dumping:\n", exports->NumberOfFunctions, exports->NumberOfNames);
-    sock_log(s, ptr_msg);
-
-    for (SIZE_T i = 0; i < exports->NumberOfNames; i++) {
-        char* name = read_null_terminated_string((UINT64)moduleInfo.Base + nameTable[i]);
-        sprintf_s(ptr_msg,
-            sizeof(ptr_msg),
-            "%s\n", name);
-        sock_log(s, ptr_msg);
-    }
-
-    free(exports);
-    free(nameTable);*/
-
-    /*for (SIZE_T i = 0; i < 16; i++) {
-        PIMAGE_DATA_DIRECTORY pDataDirectory = &ntHeader.OptionalHeader.DataDirectory[i];
-        sprintf_s(ptr_msg,
-            sizeof(ptr_msg),
-            "[?] DATA_DIRECTORY [%i] [0x%llx/%i]\n", i, (UINT64)((UINT64)moduleInfo.Base + pSection->VirtualAddress), pDataDirectory->Size);
-        sock_log(s, ptr_msg);
-        continue;
-        if (pDataDirectory->VirtualAddress == 0 || pDataDirectory->Size == 0) 
-            continue;
-        
-        char* buffer = (char*)malloc(pDataDirectory->Size);
-        krnl_read((UINT64)moduleInfo.Base + pDataDirectory->VirtualAddress, buffer, pDataDirectory->Size);
-
-        size_t data_size = sizeof(buffer);
-        size_t num_lines = (data_size + 15) / 16;
-        size_t output_size = num_lines * HEXDUMP_LINE_SIZE + 1; // +1 for null terminator
-        char* out = (char*)malloc(output_size);
-        hex_dump(buffer, sizeof(buffer), out);
-
-        sprintf_s(ptr_msg,
-            sizeof(ptr_msg),
-            "[?] DATA_DIRECTORY [%i]:\n%s\n",i, out);
-        sock_log(s, ptr_msg);
-        free(out);
-        free(buffer);
-    }*/
-
-    
-    
-
-    sprintf_s(ptr_msg,
-        sizeof(ptr_msg),
-        "[?] logging: %s\n", sectionHeader[0].Name);
-    sock_log(s, ptr_msg);
-
-
-    if (!sectionHeader[0].Name) {
-        free(sectionHeader);
-        sock_log(s, "[!] Invalid section header.\n");
-        return 2;
-    }
-
-    //sock_log(s, "Done!\n");
-
-    char* dumpDirectory = "D:\\hpayload\\dumped_modules\\";
-    size_t pathLen = strlen(dumpDirectory) + strlen(name) + 1;
-
-    char* filePath = (char*)malloc(pathLen * sizeof(char));
-    strcpy(filePath, dumpDirectory);
-    strcat(filePath, name);
-
-    FILE* file = fopen(filePath, "wb");
-    if (!file) {
-        sock_log(s, "[!] Failed to open output file.\n");
-        free(filePath);
-        free(sectionHeader);
-        return 3;
-    }
-
-    /*for (int i = 0; i < ntHeader.FileHeader.NumberOfSections; i++) {
-        //break; // skip for testing
-        PIMAGE_SECTION_HEADER pSection = &sectionHeader[i];
-        pSection->PointerToRawData = pSection->VirtualAddress;
-        pSection->SizeOfRawData = pSection->Misc.VirtualSize;
-    }*/
-    
-    unsigned char dosStub[] = {
-        0x0E, 0x1F, 0xBA, 0x0E, 0x00, 0xB4, 0x09, 0xCD, 0x21, 0xB8, 0x01, 0x4C,
-        0xCD, 0x21, 0x54, 0x68, 0x69, 0x73, 0x20, 0x70, 0x72, 0x6F, 0x67, 0x72,
-        0x61, 0x6D, 0x20, 0x63, 0x61, 0x6E, 0x6E, 0x6F, 0x74, 0x20, 0x62, 0x65,
-        0x20, 0x72, 0x75, 0x6E, 0x20, 0x69, 0x6E, 0x20, 0x44, 0x4F, 0x53, 0x20,
-        0x6D, 0x6F, 0x64, 0x65, 0x2E, 0x0D, 0x0D, 0x0A, 0x24, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00
-    };
-
-    SIZE_T richHeaderAndPadSize = dosHeader.e_lfanew - (sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub));
-    char* richHeaderAndPad = (char*)malloc(richHeaderAndPadSize);
-    if (krnl_read((UINT64)moduleInfo.Base + (sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub)), richHeaderAndPad, richHeaderAndPadSize) != 0) {
-        sock_log(s, "[!] Failed to read rich header!\n");
-
-        free(richHeaderAndPad);
-        free(filePath);
-        free(sectionHeader);
-        return 2;
-    }
-
-    SIZE_T headerSize = sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub) + richHeaderAndPadSize + sizeof(IMAGE_NT_HEADERS64) + sectionHeaderTableSize;
-    char* headers = (char*)malloc(headerSize);
-    memcpy(headers, &dosHeader, sizeof(IMAGE_DOS_HEADER));
-    memcpy(headers + sizeof(IMAGE_DOS_HEADER), dosStub, sizeof(dosStub));
-    memcpy(headers + sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub), richHeaderAndPad, richHeaderAndPadSize);
-    memcpy(headers + sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub) + richHeaderAndPadSize, &ntHeader, sizeof(IMAGE_NT_HEADERS64));
-    memcpy(headers + sizeof(IMAGE_DOS_HEADER) + sizeof(dosStub) + richHeaderAndPadSize + sizeof(IMAGE_NT_HEADERS64), sectionHeader, sectionHeaderTableSize);
-    
-    if (fwrite(headers, 1, headerSize, file) != headerSize) {
-        sock_log(s, "[!] Failed to headers write to output file.\n");
-        fclose(file);
-        free(headers);
-        free(richHeaderAndPad);
-        free(filePath);
-        free(sectionHeader);
-        return 4;
-    }
-
-    
-    
-    free(headers);
-    free(richHeaderAndPad);
-    
-
-
-    //PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)headersBuffer;
-    //PIMAGE_NT_HEADERS64 pNtHeaders = (PIMAGE_NT_HEADERS64)((char*)pDosHeader + pDosHeader->e_lfanew);
-    //PIMAGE_SECTION_HEADER pSectionHeaders = (PIMAGE_SECTION_HEADER)((char*)&pNtHeaders->OptionalHeader + pNtHeaders->FileHeader.SizeOfOptionalHeader);
-
-    sock_log(s, "[?] Dumping sections...\n");
-    for (int i = 0; i < ntHeader.FileHeader.NumberOfSections; i++) { // TODO: segments being doubled for some reason?? also, file header is malformed, check header code
-        //break; // skip for testing
-        PIMAGE_SECTION_HEADER pSection = &sectionHeader[i];
-        UINT64 sectionStart = (UINT64)((UINT64)moduleInfo.Base + pSection->VirtualAddress);
-        SIZE_T sectionSize = pSection->Misc.VirtualSize;
-        
-        if ((pSection->Characteristics & IMAGE_SCN_MEM_DISCARDABLE)) { // look into MiGetPteAddress, perhaps remnants of discarded sections can be found? otherwise we're out of luck
-            //if (!(forceDumpInit && strcmp(pSection->Name, "INIT") == 0)) {
-                sprintf_s(ptr_msg,
-                    sizeof(ptr_msg),
-                    "[?] [%s/0x%llx/%zu] (discarded)\n", pSection->Name, sectionStart, sectionSize);
-                sock_log(s, ptr_msg);
-                continue;
-            //}
-        }
-
-        sprintf_s(ptr_msg,
-            sizeof(ptr_msg),
-            "[?] [%s/0x%llx/%zu]\n", pSection->Name, sectionStart, sectionSize);
-        sock_log(s, ptr_msg);
- 
-        /*if (strcmp(pSection->Name, "GFIDS") == 0 || strcmp(pSection->Name, ".rsrc") == 0 || strcmp(pSection->Name, ".reloc") == 0) {
-            //sock_log(s, "[?] Skipping GFIDS.\n");
-            sprintf_s(ptr_msg,
-                sizeof(ptr_msg),
-                "[?] Skipping %s (to avoid crash?)\n", pSection->Name);
-            sock_log(s, ptr_msg);
-            char* nullSection = (char*)calloc(sectionSize, 1);
-            fwrite(nullSection, 1, sizeof(nullSection), file);
-            free(nullSection);
-            continue;
-        }*/
-
-        char* sectionBuffer = (char*)malloc(sectionSize);
-        if (!sectionBuffer) {
-            sock_log(s, "[!] Failed to allocation section buffer.\n");
-            fclose(file);
-            free(sectionHeader);
-            return 5;
-        }
-
-        if (krnl_read(sectionStart, sectionBuffer, sectionSize) != 0) {
-            sock_log(s, "[!] Failed to read section.\n");
-            free(sectionBuffer);
-            fclose(file);
-            free(sectionHeader);
-            return 6;
-        }
-
-        
-        fseek(file, pSection->VirtualAddress, SEEK_SET);
-        if (fwrite(sectionBuffer, 1, sectionSize, file) != sectionSize) {
-            sock_log(s, "[!] Failed to write section to file\n");
-            free(sectionBuffer);
-            fclose(file);
-            free(sectionHeader);
-            return 7;
-        }
-
-        free(sectionBuffer);
-        
-    }
-
-    fclose(file);
-    free(filePath);
-    free(sectionHeader);
-    return 0;
-}
-
-
 int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInfo) {
+
+    // skipping, i need to get the image headers reading
+    if (strcmp(name, "ntoskrnl.exe") == 0)
+        return 1337; 
 
     CHAR ptr_msg[1024] = { 0 };
     PVOID pRegBuffer = NULL;
@@ -397,43 +108,31 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
     IMAGE_DOS_HEADER dosHeader;
 
 
-    int ret = ioring_read(pRegBuffer, (ULONG64)moduleInfo.Base, &dosHeader, sizeof(IMAGE_DOS_HEADER));
+    int ret = krnl_read_s((ULONG64)moduleInfo.Base, &dosHeader, sizeof(IMAGE_DOS_HEADER));
     if (ret != 0) {
-        sock_log(s, "[!] ioring_read failed whilst reading DOS Header!\n");
+        sock_log(s, "[!] Failed to read DOS Header (incorrect permissions?)!\n");
         return 4;
     }
     if (!dosHeader.e_lfanew) {
         sock_log(s, "[!] Invalid DOS Header.\n");
         return 4;
     }
-    if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE) { // ignore dos header?
+    if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE) {
         sprintf_s(ptr_msg,
             sizeof(ptr_msg),
             "[!] Invalid DOS Header signature: 0x%lx\n", dosHeader.e_magic);
         sock_log(s, ptr_msg);
         return 5;
     }
-    sock_log(s, "[?] DOS Header read and validated.\n");
-    if (name == "ntoskrnl.exe") { // will take a look at once driver dumper works for normal drivers
-        sock_log(s, "[?] Skipping ntoskrnl.exe (i think it does nifty shit)\n");
-        return 9;
-    }
+    sock_log(s, "[?] DOS Header validated.\n");
 
     IMAGE_NT_HEADERS ntHeader;
     sprintf_s(ptr_msg,
         sizeof(ptr_msg),
         "[?] Reading NT Header from 0x%llx... ", (ULONG64)moduleInfo.Base + dosHeader.e_lfanew);
     sock_log(s, ptr_msg);
-    krnl_read((ULONG64)((ULONG64)moduleInfo.Base + dosHeader.e_lfanew), &ntHeader, sizeof(IMAGE_NT_HEADERS));
+    krnl_read_s((ULONG64)moduleInfo.Base + dosHeader.e_lfanew, &ntHeader, sizeof(IMAGE_NT_HEADERS));
     sock_log(s, "Done!\n");
-
-    //char ntHexDump[4096*2] = { 0 };
-    //hex_dump(&ntHeader, sizeof(ntHeader), ntHexDump);
-
-    //sprintf_s(ptr_msg,
-    //    sizeof(ptr_msg),
-    //    "[?] NT Header dump:\n%s\n", ntHexDump);
-    //sock_log(s, ptr_msg);
 
     if (ntHeader.Signature != IMAGE_NT_SIGNATURE) {
         sprintf_s(ptr_msg,
@@ -449,7 +148,7 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
         return 6;
     }
 
-    sock_log(s, "[?] NT Header read and validated.\n");
+    sock_log(s, "[?] NT Header validated.\n");
 
     sprintf_s(ptr_msg,
         sizeof(ptr_msg),
@@ -462,40 +161,17 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
         return 2;
     }
 
-    BYTE* testBuffer = (BYTE*)VirtualAlloc(NULL, 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-
-
     ULONG64 base = moduleInfo.Base;
-    sock_log(s, "[!] Testing read.\n");
-    krnl_read(base + 0xa000, testBuffer, 1);
-    sock_log(s, "[!] Test read success, dumping.\n");
-    char ntHexDump[4096 * 2] = { 0 };
-    hex_dump(testBuffer, sizeof(testBuffer), ntHexDump);
-
-    sprintf_s(ptr_msg,
-        sizeof(ptr_msg),
-        "%s\n", ntHexDump);
-    sock_log(s, ptr_msg);
-
-    for (UINT32 page = 0x0; page < ntHeader.OptionalHeader.SizeOfImage; page += 0x1000) {
-        if (page > 0x8000) // doing a little dump first, will fix this later
-            break;
+    for (UINT32 page = 0x0; page < imageSize; page += 0x1000) {
         ULONG64 readAddr = base + page;
         sprintf_s(ptr_msg,
             sizeof(ptr_msg),
-            "[?] Reading page from 0x%llx... ", readAddr);
+            "\r[?] Dumping page [0x%llx]", readAddr);
         sock_log(s, ptr_msg);
-        int res = krnl_read(readAddr, pModuleBuffer + page, 0x1000);
-        sock_log(s, "Done!\n");
-        if (res != 0) {
-            sprintf_s(ptr_msg,
-                sizeof(ptr_msg),
-                "[!] Failed to read section: 0x%lx [ERR:0x%llx]\n", page, res);
-            sock_log(s, ptr_msg);
-        }
+        krnl_read_s(readAddr, pModuleBuffer + page, 0x1000);
     }
 
-    sock_log(s, "[?] horray.\n");
+    sock_log(s, "[?] Done!.\n");
 
     PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)pModuleBuffer;
     if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
@@ -507,8 +183,6 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
         return 7;
     }
 
-    sock_log(s, "[?] DOS Header validated.\n");
-
     PIMAGE_NT_HEADERS64 pNtHeader = (PIMAGE_NT_HEADERS64)(pModuleBuffer + pDosHeader->e_lfanew);
     if (pNtHeader->Signature != IMAGE_NT_SIGNATURE) {
         sprintf_s(ptr_msg,
@@ -519,12 +193,8 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
         return 8;
     }
 
-    sock_log(s, "[?] NT Header validated.\n");
-
     PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(pNtHeader);
-
-    for (WORD i = 0; i < pNtHeader->FileHeader.NumberOfSections; ++i, section++) {
-
+    for (WORD i = 0; i < ntHeader.FileHeader.NumberOfSections; ++i, section++) {
         section->PointerToRawData = section->VirtualAddress;
         section->SizeOfRawData = section->Misc.VirtualSize;
 
@@ -535,8 +205,6 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
     }
 
     sock_log(s, "[?] Done.\n");
-
-    //char* dumpDirectory = "D:\\hpayload\\dumped_modules\\";
 
     size_t pathLen = strlen(dumpDirectory) + strlen(name) + 1;
 
@@ -556,7 +224,7 @@ int dump_kmodule(SOCKET s, char* name, SYSTEM_MODULE_INFORMATION_ENTRY moduleInf
     }
     sock_log(s, "Done!\n");
 
-    if (!WriteFile(hFile, pModuleBuffer, 0x8000, NULL, NULL)) {
+    if (!WriteFile(hFile, pModuleBuffer, imageSize, NULL, NULL)) {
         VirtualFree(pModuleBuffer, 0, MEM_RELEASE);
         return GetLastError();
     }
@@ -699,7 +367,7 @@ void dump_kmodules(SOCKET sock) {
                     pModuleInfo->Module[i].Base,
                     pModuleInfo->Module[i].Size);
                 sock_log(sock, ptr_msg);
-                int res = dump_kmodule2(sock, moduleName, pModuleInfo->Module[i], forceDumpInit);
+                int res = dump_kmodule(sock, moduleName, pModuleInfo->Module[i], forceDumpInit);
                 //int res = 1;
                 if (res == 0) {
                     sprintf_s(ptr_msg,
